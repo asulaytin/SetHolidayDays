8e9947a0ad90740991df35b8f5741c7bc6f7cc45
15055280 <15055280@students.southwales.ac.uk>
2020-02-11 20:38:18 +0300

    Set Holiday Days

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class HolidayDates1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HolidayDates1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.HolidaysList = new System.Windows.Forms.ListBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.EndDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.StartDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SearchDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.picDelete = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.UKBH = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BHEVENT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BHDAY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BHDATE = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picDelete)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UKBH)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(10, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Starts From:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(10, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ends:";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(16, 130);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(104, 41);
+            this.btnSubmit.TabIndex = 8;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // HolidaysList
+            // 
+            this.HolidaysList.FormattingEnabled = true;
+            this.HolidaysList.Location = new System.Drawing.Point(16, 177);
+            this.HolidaysList.Name = "HolidaysList";
+            this.HolidaysList.Size = new System.Drawing.Size(289, 160);
+            this.HolidaysList.TabIndex = 9;
+            this.HolidaysList.SelectedIndexChanged += new System.EventHandler(this.HolidaysList_SelectedIndexChanged);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(9, 27);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(104, 33);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.EndDatePicker);
+            this.groupBox1.Controls.Add(this.StartDatePicker);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 17);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(452, 94);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Holiday Dates";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(352, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(90, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(278, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 22);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Name:";
+            // 
+            // EndDatePicker
+            // 
+            this.EndDatePicker.Location = new System.Drawing.Point(144, 61);
+            this.EndDatePicker.Name = "EndDatePicker";
+            this.EndDatePicker.Size = new System.Drawing.Size(131, 20);
+            this.EndDatePicker.TabIndex = 9;
+            this.EndDatePicker.ValueChanged += new System.EventHandler(this.EndDatePicker_ValueChanged);
+            // 
+            // StartDatePicker
+            // 
+            this.StartDatePicker.Location = new System.Drawing.Point(144, 19);
+            this.StartDatePicker.Name = "StartDatePicker";
+            this.StartDatePicker.Size = new System.Drawing.Size(131, 20);
+            this.StartDatePicker.TabIndex = 8;
+            this.StartDatePicker.ValueChanged += new System.EventHandler(this.StartDatePicker_ValueChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.SearchDatePicker);
+            this.groupBox2.Controls.Add(this.btnSearch);
+            this.groupBox2.Location = new System.Drawing.Point(21, 352);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(450, 74);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search for a holiday";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(119, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Starts From:";
+            // 
+            // SearchDatePicker
+            // 
+            this.SearchDatePicker.Location = new System.Drawing.Point(205, 33);
+            this.SearchDatePicker.Name = "SearchDatePicker";
+            this.SearchDatePicker.Size = new System.Drawing.Size(140, 20);
+            this.SearchDatePicker.TabIndex = 17;
+            this.SearchDatePicker.ValueChanged += new System.EventHandler(this.SearchDatePicker_ValueChanged);
+            // 
+            // picDelete
+            // 
+            this.picDelete.Image = ((System.Drawing.Image)(resources.GetObject("picDelete.Image")));
+            this.picDelete.Location = new System.Drawing.Point(311, 177);
+            this.picDelete.Name = "picDelete";
+            this.picDelete.Size = new System.Drawing.Size(157, 160);
+            this.picDelete.TabIndex = 16;
+            this.picDelete.TabStop = false;
+            this.picDelete.Click += new System.EventHandler(this.picDelete_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(875, 396);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(82, 43);
+            this.btnExit.TabIndex = 17;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // UKBH
+            // 
+            this.UKBH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.UKBH.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.BHDATE,
+            this.BHDAY,
+            this.BHEVENT});
+            this.UKBH.Location = new System.Drawing.Point(23, 42);
+            this.UKBH.Name = "UKBH";
+            this.UKBH.Size = new System.Drawing.Size(411, 199);
+            this.UKBH.TabIndex = 19;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.UKBH);
+            this.groupBox3.Location = new System.Drawing.Point(501, 17);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(452, 259);
+            this.groupBox3.TabIndex = 20;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Bank Holidays";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label5.Location = new System.Drawing.Point(6, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(267, 22);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Upcoming 2017 Bank Holidays:";
+            // 
+            // BHEVENT
+            // 
+            this.BHEVENT.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.BHEVENT.HeaderText = "Event";
+            this.BHEVENT.Name = "BHEVENT";
+            this.BHEVENT.ReadOnly = true;
+            // 
+            // BHDAY
+            // 
+            this.BHDAY.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.BHDAY.HeaderText = "Day";
+            this.BHDAY.Name = "BHDAY";
+            this.BHDAY.ReadOnly = true;
+            // 
+            // BHDATE
+            // 
+            this.BHDATE.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.BHDATE.HeaderText = "Date";
+            this.BHDATE.MinimumWidth = 100;
+            this.BHDATE.Name = "BHDATE";
+            this.BHDATE.ReadOnly = true;
+            // 
+            // HolidayDates1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 451);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.picDelete);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.HolidaysList);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "HolidayDates1";
+            this.Text = "Holiday Dates";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picDelete)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UKBH)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ListBox HolidaysList;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox picDelete;
+        private System.Windows.Forms.DateTimePicker StartDatePicker;
+        private System.Windows.Forms.DateTimePicker EndDatePicker;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker SearchDatePicker;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView UKBH;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BHDATE;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BHDAY;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BHEVENT;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class HolidayDates1 : Form
+    {
+        private object cout;
+        private bool unique;
+        private int temp;
+        private int j;
+
+        string ListItems;
+        string StartDate;
+        string EndDate;
+        
+        const string BH_DATE = "Date";
+        const string BH_DAY = "Day";
+        const string BH_EVENT = "Event";
+
+
+        public HolidayDates1()
+        {
+            InitializeComponent();
+
+            string ListItems;
+            string StartDate;
+            string EndDate;
+
+            //UK standerd Bank Holidays list for 2017:
+            UKBH.Rows.Add("14 April", "Friday", "Good Friday");
+            UKBH.Rows.Add("17 April", "Monday", "Easter Monday");
+            UKBH.Rows.Add("1 May", "Monday", "Early May Bank Holiday");
+            UKBH.Rows.Add("29 May", "Monday", "Spring Bank Holiday");
+            UKBH.Rows.Add("27 August", "Monday", "Summer bank holiday");
+            UKBH.Rows.Add("25 December", "Tuesday", "Christmas Day");
+            UKBH.Rows.Add("26 December", "Wednesday", "Boxing Day");
+
+
+        }
+
+        //The Start date picker:
+        private void StartDatePicker_ValueChanged(object sender, EventArgs e)
+        {
+            string StartDate;
+        }
+
+        //The End date picker:
+        private void EndDatePicker_ValueChanged(object sender, EventArgs e)
+        {
+            string EndDate;
+        }
+
+        //Submit button:
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+                 string date = StartDatePicker.Text + "  To  " + EndDatePicker.Text + "  " + textBox1.Text;
+                 if (HolidaysList.Items.Contains(date))
+                 {
+                     MessageBox.Show("Error! The holiday is already created");
+                 }
+                 else
+                 {
+                     HolidaysList.Items.Add(date);
+                 }
+        }
+
+
+        //Holidays List:
+        private void HolidaysList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           string ListItems;
+            
+        }
+
+
+
+        //  Bin pictuer button: 
+        private void picDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("You are about to remove:\n\n " + HolidaysList.SelectedItem + " \n\nFrom the List. Click OK to proceed",
+    "Warning!", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if (dialogResult == DialogResult.OK)
+            {
+                HolidaysList.Items.Remove(HolidaysList.SelectedItem);
+            }
+        }
+
+        //The search date picker:
+        private void SearchDatePicker_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Search button:
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            HolidaysList.SelectedItems.Clear();
+            for (int i = HolidaysList.Items.Count - 1; i >= 0; i++)
+            {
+                if (HolidaysList.Items[i].ToString().Contains(SearchDatePicker.Text))
+                {
+                    HolidaysList.SetSelected(i, true);
+                   MessageBox.Show("Yes, there is a holiday on  " + SearchDatePicker.Text);
+                }
+                else
+                {
+                     //HolidaysList.SetSelected(i, false);
+                    MessageBox.Show("Sorry! There's not a holiday on  " + SearchDatePicker.Text);
+                }
+            }
+        }
+
+        
+
+
+        //Exit button:
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+
+    }
+}
